bc80019ee6e19d37d5effc3e3be3651901df6d01
olegchursin <olegchursin@gmail.com>
2018-04-10 17:13:17 -0400

    add reverse-string

---
+++ 01-reverse-string/index.js
@@ -0,0 +1,20 @@
+// Given a string as an argument, return a reversed string.
+
+// Solution 1
+const reversestring1 = (string) => {
+  return string
+  .split('')
+  .reverse()
+  .join('');
+}
+
+// Solution 2
+const reversestring2 = (string) => {
+  let reversed = '';
+
+  for (let char of string) {
+    reversed = char + reversed;
+  }
+
+  return reversed;
+}
